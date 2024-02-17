76cab19a879417435ead8bf4bcab3b37778b90ea
jaybartels01 <160359719+jaybartels01@users.noreply.github.com>
2024-02-17 13:54:59 -0800

    changed Logo to HelloWorld page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <Image
           className={styles.logo}
           src="/next.svg"
-          alt="Next.js Logo"
+          alt="HelloWorld.js Logo"
           width={180}
           height={37}
           priority
